802d740fa6d94c0f80bd02363449fca7e585c98c
iko4akov <iko4akov@gmail.com>
2023-04-13 17:43:46 +0300

    add test_hide_num_card

--- tests/test_hide_number_card.py
+++ tests/test_hide_number_card.py
@@ -10,3 +10,30 @@ def test_hide_number_card_valid():
     assert hide_number_card(info_1) == 'Testservice 1596 83** **** 5199'
     assert hide_number_card(info_2) == 'Test **6952'
     assert hide_number_card(info_3) == 'Test service 6831 98** **** 7658'
+
+def test_hide_number_card_invalid():
+    """Test case invalid number"""
+
+    info_1 = 'Testservice'
+    info_2 = 'Test 6952'
+    info_3 = 'Test service'
+    info_4 = ''
+    info_5 = '123'
+
+
+    assert hide_number_card(info_1) == 'Testservice '
+    assert hide_number_card(info_2) == 'Test '
+    assert hide_number_card(info_3) == 'Test service '
+    assert hide_number_card(info_4) == ''
+    assert hide_number_card(info_5) == ''
+
+
+def test_hide_number_card_error():
+    """Test case return error """
+    info_6 = 123
+    try:
+        hide_number_card(info_6) == ''
+        assert False,  'Expected an exception to be raised'
+
+    except AttributeError:
+        pass
